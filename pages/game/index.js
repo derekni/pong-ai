e8275302d1dfc2ce82d317fef0b339e9a1e0fdc8
derekni <dan82@cornell.com>
2022-10-25 23:22:47 -0400

    random set up

--- pages/game/index.js
+++ pages/game/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,11 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to the Pong AI!</h1>
-
-        <a href="game" className={styles.card}>
-          <h2>Play now &rarr;</h2>
-        </a>
+        <h1 className={styles.title}>Game</h1>
       </main>
 
       <footer className={styles.footer}>
